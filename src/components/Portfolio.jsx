8d74eec306005d011a069c5f7129541d6a8bd64e
aadii1502 <129250086+aadii1502@users.noreply.github.com>
2023-10-22 18:27:47 +0530

    Update Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,28 +1,37 @@
-import React from 'react'
+import React , {useEffect,useRef} from 'react'
 import image1 from '../assets/p-1.png'
 import image2 from '../assets/p-2.png'
 import image3 from '../assets/p-3.png'
 import image4 from '../assets/p-4.png'
 import image5 from '../assets/p-5.png'
-import { motion } from "framer-motion";
-import { useInView } from "react-intersection-observer";
+import { motion,useInView,useAnimation } from "framer-motion";
+
 const Portfolio = () => {
-    const { ref, inView } = useInView({
-        triggerOnce: true,
-      });
+    const ref = useRef(null);
+    const isInView = useInView(ref,{once:true});
+    const mainControls = useAnimation();
+    useEffect (() => {
+        if(isInView){
+         mainControls.start("visible");
+        }
+    },[isInView])
   return (
-    <div id='portfolio'>
+    <div id='portfolio' ref={ref}>
         <div>
-            
-            <div className=' bg-gray-900'>
+             <div className=' bg-gray-900'>
             <h2 className='titleBackground border border-b-[#00df9a] w-fit border-t-0 border-l-0 border-r-0'>PORTFOLIO</h2>
             <h1 className=' text-white mt-10 text-2xl flex justify-center font-bold'>MY PROJECTS</h1>
             <div className='grid w-full sm:grid-cols-2 md:grid-cols-3 gap-10 px-10 sm:px-10 py-10'>
                 <motion.div
-                ref={ref}
-                initial={{ opacity: 0, y: 100 }}
-                animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 100 }}
-                transition={{ duration: 1}}
+                variants={
+                    {
+                      hidden:{opacity:0,y:75},
+                      visible:{opacity:1,y:0},
+                    }
+                }
+                initial="hidden"
+                animate={mainControls}
+                transition={{ duration: 1 , delay:0.25}}
                 className=' bg-gray-100 h-full rounded-xl shadow-sm shadow-[#00df9a]'>
                   <img className='rounded-xl' src={image1} alt="" />
                   <h1 className=' text-black px-4 pt-2 flex justify-center text-xl font-semibold'>RELIEF+</h1>
@@ -30,10 +39,15 @@ const Portfolio = () => {
                 </motion.div>
 
                 <motion.div
-                ref={ref}
-        initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 100 }}
-        transition={{ duration: 1 ,delay:0.5 }}
+                 variants={
+                    {
+                      hidden:{opacity:0,y:75},
+                      visible:{opacity:1,y:0},
+                    }
+                }
+                initial="hidden"
+                animate={mainControls}
+                transition={{ duration: 1 , delay:0.5}}
                 className=' bg-gray-100 h-full rounded-xl shadow-sm shadow-[#00df9a]'>
                   <img className='rounded-xl' src={image2} alt="" />
                   <h1 className=' text-black px-4 pt-2 flex justify-center text-xl font-semibold'>DESIGN SOL</h1>
@@ -42,10 +56,15 @@ const Portfolio = () => {
 
 
                 <motion.div
-                ref={ref}
-        initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 100 }}
-        transition={{ duration: 1,delay:1 }}
+                 variants={
+                    {
+                      hidden:{opacity:0,y:75},
+                      visible:{opacity:1,y:0},
+                    }
+                }
+                initial="hidden"
+                animate={mainControls}
+                transition={{ duration: 1 , delay:0.75}}
                 className=' bg-gray-100 h-full rounded-xl shadow-sm shadow-[#00df9a]'>
                   <img className='rounded-xl' src={image3} alt="" />
                   <h1 className=' text-black px-4 pt-2 flex justify-center text-xl font-semibold'>E-COMMERCE</h1>
@@ -54,10 +73,15 @@ const Portfolio = () => {
 
 
                 <motion.div
-                ref={ref}
-        initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 100 }}
-        transition={{ duration: 1,delay:1.5 }}
+                 variants={
+                    {
+                      hidden:{opacity:0,y:75},
+                      visible:{opacity:1,y:0},
+                    }
+                }
+                initial="hidden"
+                animate={mainControls}
+                transition={{ duration: 1 , delay:1}}
                 className=' bg-gray-100 h-full rounded-xl shadow-sm shadow-[#00df9a]'>
                   <img className='rounded-xl' src={image4} alt="" />
                   <h1 className=' text-black px-4 pt-2 flex justify-center text-xl font-semibold'>BANKMANIA</h1>
@@ -66,10 +90,15 @@ const Portfolio = () => {
 
 
                 <motion.div
-                ref={ref}
-        initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 100 }}
-        transition={{ duration: 1,delay:2 }}
+                 variants={
+                    {
+                      hidden:{opacity:0,y:75},
+                      visible:{opacity:1,y:0},
+                    }
+                }
+                initial="hidden"
+                animate={mainControls}
+                transition={{ duration: 1 , delay:1.25}}
                 className=' bg-gray-100 h-full rounded-xl shadow-sm shadow-[#00df9a]'>
                   <img className='rounded-xl' src={image5} alt="" />
                   <h1 className=' text-black px-4 pt-2 flex justify-center text-xl font-semibold'>UNITY LEARN</h1>
@@ -78,10 +107,15 @@ const Portfolio = () => {
 
 
                 <motion.div
-                ref={ref}
-        initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 100 }}
-        transition={{ duration: 1,delay:2.5 }}
+                 variants={
+                    {
+                      hidden:{opacity:0,y:75},
+                      visible:{opacity:1,y:0},
+                    }
+                }
+                initial="hidden"
+                animate={mainControls}
+                transition={{ duration: 1, delay:1.5}}
                 className=' bg-gray-100 h-full rounded-xl shadow-sm shadow-[#00df9a]'>
                   <img className='rounded-xl' src={image4} alt="" />
                   <h1 className=' text-black px-4 pt-2 flex justify-center text-xl font-bold'>RELIEF+</h1>
@@ -97,4 +131,4 @@ const Portfolio = () => {
   )
 }
 
-export default Portfolio
+export default Portfolio;

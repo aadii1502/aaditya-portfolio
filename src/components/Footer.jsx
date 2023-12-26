e815c706174d029aa7803024e276923171594949
aadii1502 <129250086+aadii1502@users.noreply.github.com>
2023-12-26 22:16:21 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div>
         <div className=' flex font-bold uppercase font-serif flex-wrap justify-center bg-black text-white py-2'>
-            Made with love by Aaditya.
+            
         </div>
     </div>
   )
